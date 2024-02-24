0d3c546d85ebf082204ebc520d301cf0a3252309
Mahiz01 <124418649+Mahiz01@users.noreply.github.com>
2024-02-25 01:53:20 +0530

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,14 @@
+// const btn=document.querySelector(".btn1");
+// btn.addEventListener("click",()=>{
+//     alert("Redirecting To another page")});
+
+
+const res=()=>{
+    alert("Redirecting to other site");
+};
+const res1=()=>{
+    alert("Redirecting to other site");
+};
+const res2=()=>{
+    alert("THANKS FOR SHOWING INTEREST TOWARDS MY PORTFOLIO");
+};
